0f08654e835269a6fe3e85b7a3d4b8a9aacab379
Basix <basix@basix.tech>
2020-01-08 06:07:26 +0900

    Add tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,15 @@
-
+use crate::parser::identifiers::*;
 #[test]
-fn it_works() {
-  assert_eq!(2 + 2, 4);
+fn can_parse_lc_ident_full() {
+  assert_eq!(
+    lc_ident_full("decryptedMessage#1f814f1f"),
+    Ok((
+      "",
+      (
+        String::from("decryptedMessage"),
+        None,
+        Some(String::from("1f814f1f"))
+      )
+    ))
+  )
 }
